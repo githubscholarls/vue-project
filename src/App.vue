f5ae165aedb584736a4c9041cfd5bfc4aa1d6ee5
lishuai_schoalr <2363502880@qq.com>
2024-05-29 15:05:13 +0800

    请求下载压缩包

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,48 @@ function toAboutView(){
   router.push({ name: 'adName', params: { comp: 'erina',id:'333' } })
 }
 
+function downloadzq(){
+  console.log('开始下载');
+  fetch('https://zhengqi.lsscholar.cn/Admin/GetPersonPic', {
+  method: 'POST',
+  headers: {
+    'accept': '*/*',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJ1c2VybmFtZSI6ImFkbWluIiwidXNlcnBhc3N3b3JkIjoiMjEyMzJmMjk3YTU3YTVhNzQzODk0YTBlNGE4MDFmYzMiLCJ1c2VyaWQiOiIxMDAiLCJyb2xldHlwZSI6IjIiLCJ2aWxsYWdlaWQiOiIwIiwidGltZW5vdGljZSI6IjE3MTQwMjgwNzMiLCJuYmYiOjE3MTQwMjgwNzMsImV4cCI6MTc0NTU2NDA3MywiaXNzIjoic2l0ZUlzc3VlciIsImF1ZCI6InNpdGVBdWRpZW5jZSJ9.R5zfID7MuiYsTnXwEh2zOngxl1-fZ6IMy4xNt9fofSM',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    id: 12,
+    type: 0
+  })
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.blob(); // 将响应转换为Blob对象
+})
+.then(blob => {
+  // 创建临时URL
+  const url = URL.createObjectURL(blob);
+  
+  // 创建一个链接
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'images.zip';
+  
+  // 将链接添加到页面并模拟点击
+  document.body.appendChild(a);
+  a.click();
 
+  // 清理
+  window.URL.revokeObjectURL(url);
+})
+.catch(error => {
+  console.error('There has been a problem with your fetch operation:', error);
+});
+
+}
 
 
 
@@ -92,6 +133,9 @@ function toAboutView(){
       <router-link to="/re/redirect3-about"
         >router-link 重定向路由 /re/redirect3-about</router-link
       >
+    </li>
+    <li id="download" @click="downloadzq()">
+      下载压缩包
     </li>
   </ul>
 <p>头部内容结束</p>
